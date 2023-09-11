6b0539680b18d7a3d5a62eb84254a77722f6df40
Tarsis <br.tmvdl@gmail.com>
2023-09-11 11:29:36 -0300

    Updating tests files

--- tests/curl.sh
+++ tests/curl.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/sh
 
+# api="https://api.br-tmvdl.workers.dev"
+
+api="http://127.0.0.1:8787"
+
 method="${1}"
 
 path="${2}"
 
 data="${3}"
 
-curl -sL -X "${method}" "http://127.0.0.1:8787/${path}" --data-raw "${data}" | jq
+curl -sL -X "${method}" "${api}/${path}" --data-raw "${data}" | jq
