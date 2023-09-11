fde98bcb2790273b01e3c7e84486f6331c6f6c34
Tarsis <br.tmvdl@gmail.com>
2023-09-11 11:24:11 -0300

    Updating app server file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import { CloudflareRequest, CloudflareResponse } from '@brtmvdl/backend/cloudflare'
-
 import app from './routes'
 
 export default {
   async fetch(request) {
     const req = new CloudflareRequest(request)
     const res = new CloudflareResponse(req)
+    await req.parseProperties()
     return app.run(req, res).getResponse()
   }
 }
