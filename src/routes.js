15237f57eb883c20b9e505d160f1a215ce5ca31f
Tarsis <br.tmvdl@gmail.com>
2023-09-13 09:27:41 -0300

    Adding routes files

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 import { Router } from '@brtmvdl/backend'
 import { Logger } from '@brtmvdl/logger'
+import { Database } from '@brtmvdl/database'
 
 const router = new Router()
 const logger = new Logger('AppLogger')
+
+const db = new Database(process.env.DATABASE_URL)
+
+db.type(Database.POSTGRESQL)
+
+router.get('/api/namazon/products/list', (req, res) => res.setJSON({ id: Date.now(), endpoint: '/api/namazon/products/list' }))
+
+router.post('/api/namazon/addresses/list', (req, res) => res.setJSON({ id: Date.now(), endpoint: '/api/namazon/addresses/list' }))
+
+router.post('/api/namazon/payments/list', (req, res) => res.setJSON({ id: Date.now(), endpoint: '/api/namazon/payments/list' }))
 
 router.post('/login', (req, res) => {
   logger.log('login', { req, res })
