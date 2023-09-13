161cdb538345aa912d42748264acadf874f5acd6
Tarsis <br.tmvdl@gmail.com>
2023-09-13 16:18:33 -0300

    Adding MOCKED api files

---
+++ src/api/home-workout/index.js
@@ -0,0 +1,45 @@
+
+export const challenges = () => ([
+  {
+    "id": 1,
+    "title": "Abs beginner",
+    "last_time": 1694355479597,
+    "level": 1,
+    "workouts": [1, 2, 3]
+  },
+  {
+    "id": 2,
+    "title": "Abs advanced",
+    "last_time": 1694355478597,
+    "level": 3,
+    "workouts": [1, 2, 3, 4]
+  }
+])
+
+
+export const workouts = () => ([
+  {
+    "id": 1,
+    "title": "Jumping jacks",
+    "time": 20,
+    "workouts": null
+  },
+  {
+    "id": 2,
+    "title": "Plank",
+    "time": 20,
+    "workouts": null
+  },
+  {
+    "id": 3,
+    "title": "Mountain climber",
+    "time": null,
+    "workouts": 16
+  },
+  {
+    "id": 4,
+    "title": "Russian twist",
+    "time": null,
+    "workouts": 20
+  }
+])
---
+++ src/api/index.js
@@ -0,0 +1,1 @@
+export * as homeWorkout from './home-workout'
