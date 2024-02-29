712606a36b84783da3516e299ce35ed722223c65
Tarsis <br.tmvdl@gmail.com>
2024-02-29 16:39:48 -0300

    Updating app files

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
-import { EventEmitter } from 'events'
 
-const ee = new EventEmitter()
-
-setInterval(() => ee.emit('datetime', new Date()), 100)
-
-const config = { datetime: 0 }
-
-ee.addEventListener('datetime', (datetime = new Date()) => config.datetime = datetime.toLocaleString())
-
-export default {
-  async fetch(request) {
-    return new Response(`datetime: ${config.datetime}`)
+const server = {
+  count: 0,
+  async fetch() {
+    return new Response(`datetime: ${++server.count}`)
   }
 }
+
+export default server
